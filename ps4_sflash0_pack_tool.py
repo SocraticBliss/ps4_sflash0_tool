91d5a20bc549944b1d9721e770bfbbfa3f9db961
SocraticBliss <6451336+SocraticBliss@users.noreply.github.com>
2019-02-28 17:40:15 -0600

    Update ps4_sflash0_pack_tool.py

--- ps4_sflash0_pack_tool.py
+++ ps4_sflash0_pack_tool.py
@@ -3,27 +3,27 @@
 # Thanks to zecoxao <3
 
 '''
-    0x0 <- Header (0x1000)
-    0x1000 <- Unk (0x1000)
-    0x2000 <- MBR1 (0x1000) (for sflash0s1.cryptx3b)
-    0x3000 <- MBR2 (0x1000) (for sflash0s1.cryptx3)
-    0x4000 <- sflash0s0x32b (emc_ipl) (0x60000)
-    0x64000 <- sflash0s0x32 (emc_ipl) (0x60000)
-    0xC4000 <- sflash0s0x33 (eap_kbl) (0x80000)
-    0x144000 <- sflash0s0x34 (wifi fw) (0x80000)
-    0x1C4000 <- sflash0s0x38 (nvs) (0xC000)
-    0x1D0000 <- sflash0s0x0 (blank1) (0x30000)
-    0x200000 <- Header2 (0x1000)
-    0x201000 <- Unk2 (0x1000)
-    0x202000 <- MBR3 (0x1000) (for sflash0s1.cryptx2b)
-    0x203000 <- MBR4 (0x1000) (for sflash0s1.cryptx2)
-    0x204000 <- sflash0s1.cryptx2b (sam_ipl/secure loader) (0x3E000)
-    0x242000 <- sflash0s1.cryptx2 (sam_ipl/secure loader) (0x3E000)
-    0x280000 <- sflash0s1.cryptx1 (idata) (0x80000)
-    0x300000 <- sflash0s1.cryptx39 (bd_hrl?) (0x80000)
-    0x380000 <- sflash0s1.cryptx6 (Virtual TRM) (0x40000)
-    0x3C0000 <- sflash0s1.cryptx3b (secure kernel, secure modules) (0xCC0000)
-    0x1080000 <- sflash0s1.cryptx3 (secure kernel, secure modules) (0xCC0000)
+    0x0       <- Header (0x1000)
+    0x1000    <- Unk (0x1000)
+    0x2000    <- MBR1 (0x1000) (for sflash0s1.cryptx3b)
+    0x3000    <- MBR2 (0x1000) (for sflash0s1.cryptx3)
+    0x4000    <- sflash0s0x32b (emc_ipl) (0x60000)
+    0x64000   <- sflash0s0x32  (emc_ipl) (0x60000)
+    0xC4000   <- sflash0s0x33  (eap_kbl) (0x80000)
+    0x144000  <- sflash0s0x34  (wifi fw) (0x80000)
+    0x1C4000  <- sflash0s0x38  (nvs) (0xC000)
+    0x1D0000  <- sflash0s0x0   (blank1) (0x30000)
+    0x200000  <- Header2 (0x1000)
+    0x201000  <- Unk2 (0x1000)
+    0x202000  <- MBR3 (0x1000) (for sflash0s1.cryptx2b)
+    0x203000  <- MBR4 (0x1000) (for sflash0s1.cryptx2)
+    0x204000  <- sflash0s1.cryptx2b (sam_ipl/secure loader) (0x3E000)
+    0x242000  <- sflash0s1.cryptx2  (sam_ipl/secure loader) (0x3E000)
+    0x280000  <- sflash0s1.cryptx1  (idata) (0x80000)
+    0x300000  <- sflash0s1.cryptx39 (bd_hrl?) (0x80000)
+    0x380000  <- sflash0s1.cryptx6  (Virtual TRM) (0x40000)
+    0x3C0000  <- sflash0s1.cryptx3b (secure kernel, secure modules) (0xCC0000)
+    0x1080000 <- sflash0s1.cryptx3  (secure kernel, secure modules) (0xCC0000)
     0x1D40000 <- sflash0s1.cryptx40 (blank2) (0x2C0000)
 '''
 
@@ -60,6 +60,11 @@ def unpack(file, dir):
     with open(file, 'rb') as input:
         sflash0 = input.read()
         
+        # Check sflash0 binary...
+        if sflash0[:0x4] != 'SONY':
+            raise SystemExit('\nInvalid PS4 Sflash0 binary!')
+        
+        # Unpack each entry...
         for num, entry in enumerate(SFLASH0):
             with open('%s/%s' % (dir, SFLASH0[num][0]), 'wb') as output:
                 begin = SFLASH0[num][1]
@@ -70,6 +75,7 @@ def unpack(file, dir):
 
 def pack(dir, name):
 
+    # Pack entries into a Sflash0 binary...
     with open(name, 'wb') as output:
         for num, entry in enumerate(SFLASH0):
             with open('%s/%s' % (dir, SFLASH0[num][0]), 'rb') as input:
@@ -77,10 +83,14 @@ def pack(dir, name):
 
 def main(argc, argv):
     
+    # Print Usage Statement...
     if argc not in [2, 3]:
         raise SystemExit('\nUsage: python %s <input> [output]' % argv[0])
     
+    # If file -> Unpack || If directory -> Pack
     if os.path.isfile(argv[1]):
+        
+        # Create the custom unpack directory...
         if argc == 3:
             try:
                 os.makedirs(argv[2])
@@ -89,8 +99,11 @@ def main(argc, argv):
 
         unpack(argv[1], argv[2] if argc == 3 else '.')
     
-    else:
+    elif os.path.isdir(argv[1]):
         pack(argv[1], argv[2] if argc == 3 else 'sflash0.bin')
+        
+    else:
+        raise SystemExit('\nUsage: python %s <input> [output]' % argv[0])
     
     print('\nDone!')
 
